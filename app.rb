abb01d7e76dc40d4583dc7d01826fff7f7259243
Steve Albonico <salbonico@gmail.com>
2019-02-05 20:17:57 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -18,7 +18,8 @@ end
 get '/say/:number/:phrase' do
   @times = params[:number].to_i
   @phrase = params[:phrase]
-  @times.times {"#{@phrase}"}
+  @times.times {@phrase + @phrase}
+  @phrase
 
 end
 
