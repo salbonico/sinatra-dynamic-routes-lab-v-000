1f74dfcd00db903cd32e2cc6e1685ec750ba15d5
Steve Albonico <salbonico@gmail.com>
2019-02-05 20:39:12 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -27,5 +27,14 @@ get '/say/:number/:phrase' do
 end
 end
 
+get 'say/:word1/word2/word3/word4/word5' do
+  @word1 = params[:word1]
+  @word2 = params[:word2]
+  @word3 = params[:word3]
+  @word4 = params[:word4]
+  @word5 = params[:word5]
+  ""
+end
+
 
 end
